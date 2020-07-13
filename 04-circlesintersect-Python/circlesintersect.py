da96e6c0a522598dd7bdce2bc817c70a184a1a3e
saitejajosyula <saitejajosyula@msitprogram.net>
2020-07-13 22:39:35 +0530

    Jul 13  22:39:35

--- 04-circlesintersect-Python/circlesintersect.py
+++ 04-circlesintersect-Python/circlesintersect.py
@@ -9,7 +9,7 @@ import math
 def fun_circlesintersect(x1, y1, r1, x2, y2, r2):
 	# your code goes here
 	dist = int (math.sqrt((x2-x1)**2 + (y2-y1)**2))
-	rad = (r1+r2)**2 
+	rad = int((r1+r2)**2) 
 
 	if dist == rad:
 		return True
