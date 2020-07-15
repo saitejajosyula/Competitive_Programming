992c07ae76d5b398016d7107211519abe437bddd
saitejajosyula <saitejajosyula@msitprogram.net>
2020-07-15 11:14:43 +0530

    Jul 15  11:14:43

---
+++ Day 2/01-stacks-Python/stacks.py
@@ -0,0 +1,62 @@
+"""Add a couple methods to our LinkedList class,
+and use that to implement a Stack.
+You have 4 functions below to fill in:
+insert_first, delete_first, push, and pop.
+Think about this while you're implementing:
+why is it easier to add an "insert_first"
+function than just use "append"?"""
+
+class Element(object):
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+class LinkedList(object):
+    def __init__(self, head=None):
+        self.head = head
+        
+    def append(self, new_element):
+        current = self.head
+        if self.head:
+            while current.next:
+                current = current.next
+            current.next = new_element
+        else:
+            self.head = new_element
+
+    def insert_first(self, new_element):
+        "Insert new element as the head of the LinkedList"
+        new_element.next = self.head
+        self.head = new_element
+
+    def delete_first(self):
+        "Delete the first (head) element in the LinkedList as return it"
+        out = self.head
+        if self.head is not None:
+            self.head = self.head.next
+            out.next = None
+        return out
+
+class stack(object):
+    def __init__(self,top=None):
+        self.ll = LinkedList(top)
+
+    def push(self, new_element):
+        "Push (add) a new element onto the top of the stack"
+        if self.head is None:
+            self.head = Element(new_element)
+        else:
+            new_node = Element(new_element)
+            new_node.next = self.head
+            self.head = new_node
+
+    def pop(self):
+        "Pop (remove) the first element off the top of the stack and return it"
+        if self.head is None:
+            return None
+        else:
+            out = self.head.value
+            self.head = self.head.next
+            return out
+        
+    
