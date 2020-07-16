32cc8e33cb35acd913e7c019361d5c85b1f89eac
saitejajosyula <saitejajosyula@msitprogram.net>
2020-07-17 00:41:21 +0530

    Jul 17  00:41:21

--- 10-mostfrequencydigit-Python/mostfrequentdigit.py
+++ 10-mostfrequencydigit-Python/mostfrequentdigit.py
@@ -4,4 +4,25 @@
 
 def mostfrequentdigit(n):
 	# your code goes here
-	pass
+	
+	s = str(n)  #24
+
+	if len(s) == 1:
+		return n
+	elif len(s) == 2:
+		if s[0] <= s[1]: 
+			return int(s[0])
+		else:
+			return int(s[1])
+	else:
+
+		i = -1
+		count = 0
+
+		for j in range(len(s)-1):
+			if(s[j] == s[j+1]):
+				i = s[j]
+				count = count + 1
+
+		if count > 0:
+			return int(i)
