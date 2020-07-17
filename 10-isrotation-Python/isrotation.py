ed69521a29cbe696bdfd3eecb0732802fc736c72
saitejajosyula <saitejajosyula@msitprogram.net>
2020-07-17 23:06:38 +0530

    Jul 17  23:06:38

--- 10-isrotation-Python/isrotation.py
+++ 10-isrotation-Python/isrotation.py
@@ -7,4 +7,24 @@
 
 def isrotation(x, y):
 	# Your code goes here
-	pass
+	a = str(x)
+	b = str(y)
+
+	len1 = len(a)
+	len2 = len(b)
+
+	temp = ''
+
+	if len1 != len2:
+		return False
+
+	if a == b[::-1]:
+		return True
+
+	temp = a + a
+
+	if temp.count(b) > 0:
+
+		return True
+	else:
+		return False
