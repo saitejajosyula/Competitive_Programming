2cdafd293424c04c54044118eefdea10f375a6d9
saitejajosyula <saitejajosyula@msitprogram.net>
2020-07-18 00:06:06 +0530

    Jul 18  00:06:06

--- 02-binarysearch-Python/binarysearch.py
+++ 02-binarysearch-Python/binarysearch.py
@@ -12,4 +12,26 @@ doesn't exist in the list."""
 
 def binary_search(input_array, value):
     # Your code goes here
-    pass
+    
+    lo = 0
+    hi = len(input_array) - 1
+
+    while lo <= hi :
+
+        mid = lo + (hi - lo)/2
+
+        if input_array[mid] == value:
+            return mid
+        
+        elif input_array[mid] < value:
+
+            lo = mid - 1
+        elif input_array[mid] > value:
+
+            hi = mid + 1
+    
+    return -1
+
+
+
+
