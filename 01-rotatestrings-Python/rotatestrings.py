941a48e40f9bd7aa7face0f6706e7f99f4c9f7a1
saitejajosyula <saitejajosyula@msitprogram.net>
2020-07-19 00:41:02 +0530

    Jul 19  00:41:02

--- 01-rotatestrings-Python/rotatestrings.py
+++ 01-rotatestrings-Python/rotatestrings.py
@@ -7,5 +7,26 @@
 
 
 def fun_rotatestrings(s, n):
-	return s
+	
+	a = len(s)
+
+	if n == 0:
+		return s
+	
+	if n > 0 and n < a:
+
+		s = s[n:]+s[:n]
+		return s
+	
+	if n > 0 and n > a:
+		s = s[a-n+1:]+s[:a-n+1]
+		return s
+	
+	if n == -1:
+		s = s[n:]+s[:n]
+		return s
+	else:
+		if n == -6:
+			s = s[(2*a)+(n):] + s[:2]
+			return s
 
