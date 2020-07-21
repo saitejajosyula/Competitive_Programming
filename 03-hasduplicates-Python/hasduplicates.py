df27e2d9c6c97e267d7caeb3fbc8b59503f2586d
saitejajosyula <saitejajosyula@msitprogram.net>
2020-07-21 16:58:18 +0530

    Jul 21  16:58:17

--- 03-hasduplicates-Python/hasduplicates.py
+++ 03-hasduplicates-Python/hasduplicates.py
@@ -3,6 +3,19 @@
 # contains any duplicate values (that is, 
 # if any two values in L are equal to each other), and False otherwise.
 
+from itertools import chain
+
 def hasduplicates(L):
 	# Your code goes here
-	pass
+	lis = sum(L, [])
+	
+	res = []
+
+	for i in lis:
+		if i not in res:
+			res.append(i)
+	
+	if len(lis) == len(res):
+		return False
+	else:
+		return True
