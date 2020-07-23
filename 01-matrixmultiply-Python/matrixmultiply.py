86b0b3ca517008ba782287ca75898d40309eb1e4
saitejajosyula <saitejajosyula@msitprogram.net>
2020-07-24 00:05:32 +0530

    Jul 24  00:05:32

--- 01-matrixmultiply-Python/matrixmultiply.py
+++ 01-matrixmultiply-Python/matrixmultiply.py
@@ -7,10 +7,24 @@ import numpy as np
 
 def fun_matrixmultiply(m1, m2):
 
-    result = np.dot(m1, m2)
+    if len(m2) != len(m1[0]):
+        return None
+    
+    lis = [[0 for i in range(len(m2[0]))] for j in range(len(m1))]
 
-    for i in result:
-        print(i)
+    for i in range(len(m1)):
+
+        for j in range(len(m2[0])):
+
+            output = 0
+
+            for k in range(len(m1[0])):
+
+                output = output + m1[i][k] * m2[k][j]
+            
+            lis[i][j] = output
+    
+    return lis
 
 
 
