151c46024c920505596a4d3d84ed0d07f583e338
saitejajosyula <saitejajosyula@msitprogram.net>
2020-07-25 19:32:46 +0530

    Jul 25  19:32:46

--- 06-leastfrequentletters-Python/leastfrequentletters.py
+++ 06-leastfrequentletters-Python/leastfrequentletters.py
@@ -7,6 +7,43 @@
 # yet covered lists, sets, maps, or efficiency, you are not expected to write the most efficient solution. Finally, 
 # if s does not contain any alphabetic characters, the result should be the empty string ("")
 
+import string
+
 def leastfrequentletters(s):
 	# Your code goes here
-	pass
+
+	if len(s) == 0:
+
+		return ''
+	
+	s = s.lower()
+
+	a = string.ascii_lowercase
+
+	i = 0
+
+	m = 1000
+
+	out = ''
+
+	while i < 26:
+
+		count = s.count(a[i])
+
+		if m > count > 0 :
+
+			m = count
+
+			out = a[i]
+		
+		elif m == count:
+			out = out + a[i]
+		
+		i = i + 1
+	
+	if out == '':
+
+		return out
+	
+	return out
+	
