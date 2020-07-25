00ac0d431231804bbf33729d544d178dbc89b97d
saitejajosyula <saitejajosyula@msitprogram.net>
2020-07-25 17:09:13 +0530

    Jul 25  17:09:12

--- 06-replace-Python/replace.py
+++ 06-replace-Python/replace.py
@@ -5,5 +5,34 @@
 
 
 def fun_replace(s1, s2, s3):
-	return s1.replace(s2, s3)
+
+	a = s1.count(s2)
+
+	if a == 0:
+
+		return s1
+	
+	elif a == 1:
+
+		s = ""
+
+		first = s1.find(s2)
+		last = first + len(s2)
+
+		s = s1[0:first] + s3 + s1[last:]
+
+		return s
+	
+	else:
+
+		s = s1
+		
+		for i in range(c):
+
+			first = s.find(s2)
+			last = first + len(s2)
+
+			s = s[0:first] + s3 + s[last:]
+
+		return s
 
