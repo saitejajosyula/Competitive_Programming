45da85360c2a61f5d831a7732a3781d0d394e085
saitejajosyula <saitejajosyula@msitprogram.net>
2020-07-25 19:22:28 +0530

    Jul 25  19:22:27

--- 03-largestnumber-Java/TestJUnit.java
+++ 03-largestnumber-Java/TestJUnit.java
@@ -1,21 +1,35 @@
 /**
- * This is JUnit that tests the sleepIn method that is available in SleepIn class.
- * This contains 4 testcases.
+ * This is JUnit that tests the methods of the Clock.
+ * This contains 2 testcases.
  * 
  * Please don't run this file.
- * You can add your own test cases to this file by just copy and paste the last three 
- * lines of the code (TestCase4).
+ * You can add your own test cases to this file by just copy and 
+ * paste the last three lines of the code (TestCase2).
  * 
- * @author: Siva Sankar
+ * @author: Deepak
  */
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
+import java.beans.Transient;
+
 public class TestJUnit {
+
    @Test
    public void testCase1() {
-      assertEquals(new PythonBasics().show_excitement(), "I am super excited for this course! I am super excited for this course! I am super excited for this course! I am super excited for this course! I am super excited for this course! ");
+      largestnumber s = new largestnumber();
+      
+      assertEquals("1.", 32, s.fun_largestnumber("we have 32 dogs 3 cats"));
+      assertEquals("2.", 0, s.fun_largestnumber("we have dogs cats"));
    }
 
+   @Test
+   public void testCase2() {
+      largestnumber s = new largestnumber();
+      
+      assertEquals("1.", 17, s.fun_largestnumber("I saw 3 dogs, 17 cats, and 14 cows!"));
+      assertEquals("2.", 15, s.fun_largestnumber("wehave15dogs2cats"));
+   }
 }
+
