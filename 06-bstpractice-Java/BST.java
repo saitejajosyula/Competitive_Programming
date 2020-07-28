3781d7d601d44c9565dc3ed3e42ae3acf0f6693e
saitejajosyula <saitejajosyula@msitprogram.net>
2020-07-28 17:22:24 +0530

    Jul 28  17:22:24

--- 06-bstpractice-Java/BST.java
+++ 06-bstpractice-Java/BST.java
@@ -17,21 +17,64 @@ public class BST {
     }
 
     public void insert(int value) {
-    	// Your code goes here
+        // Your code goes here
+        insert(root, value);
     }
 
     private void insert(Node node, int value) {
-    	// Your code goes here
+        // Your code goes here
+        
+        if(value < node.value) {
+
+            if(node.left == null) {
+
+                node.left = new Node(value);
+                return;
+            } else {
+
+                insert(node.left, value);
+            }
+        } else {
+
+            if(node.right == null) {
+
+                node.right = new Node(value);
+                return;
+            } else {
+
+                insert(node.right, value);
+            }
+        }
     }
 
     public boolean search(int value) {
     	// Your code goes here
-    	return false;
+    	if(root.value == value) {
+
+            return true;
+        }
+        return search(root, value);
     }
 
     private boolean search(Node current, int value) {
     	// Your code goes here
-    	return false;
+        
+        if(current == null) {
+
+            return false;
+        } else if (current.value == value) {
+
+            return true;
+        } else {
+
+            if (value < current.value) {
+
+                return search(current.left, value);
+            } else {
+
+                return search(current.right, value);
+            }
+        }
     }
 
 }
