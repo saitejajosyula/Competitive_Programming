4f64770b718c545e6915a588509ab7c869ff3387
saitejajosyula <saitejajosyula@msitprogram.net>
2020-07-28 19:27:33 +0530

    Jul 28  19:27:33

--- 01-nthtenlyprime-Java/nthtenlyprime.java
+++ 01-nthtenlyprime-Java/nthtenlyprime.java
@@ -3,6 +3,7 @@
 // # the sum of its digits is also prime. For example, 113 is prime and 1+1+3==5 and 5 
 // # is also prime, so 113 is an Additive Prime. fun_nth_additive_prime(0) returns 2
 
+import java.util.*;
 
 class nthtenlyprime {
 
@@ -19,35 +20,40 @@ class nthtenlyprime {
 		}
 		return count == 2;
 	}
+
+	public boolean istenly(int n) {
+
+		int sum = 0;
+
+		while(n != 0) {
+
+			sum = sum + n % 10;
+
+			n = n / 10;
+		}
+
+		if (sum == 10) {
+
+			return true;
+		} else {
+
+			return false;
+		}
+	}
 	public int fun_nthtenlyprime(int n){
 
-		int count = -1;
+		List<Integer> al = new ArrayList<Integer>();
 
-		int temp = 0;
+		for(int i = 0; i < 3000; i++) {
 
-		while(count < n) {
+			if(isPrime(i) && istenly(i)) {
 
-			if(isPrime(temp)) {
-
-				int sum = 0;
-
-				int t = temp;
-
-				while(t > 0) {
-
-					sum += t % 10;
-
-					t = t / 10;
-				}
-
-				if(isPrime(sum)) {
-
-					count++;
-				}
+				al.add(i);
 			}
-			temp++;
 		}
-		return temp - 1;
+
+		return al.get(n);
+
 	}
 
 	public static void main(String [] args) {
